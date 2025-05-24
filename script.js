cc34e05feaa2067117b51b7d7f46b78c3e984a5b
Mohammed Shahaz <134310109+MohammedShahbaz123@users.noreply.github.com>
2025-05-24 20:48:03 +0530

    added onclick affect

---
+++ script.js
@@ -0,0 +1,11 @@
+const dropArrow=document.getElementById('drop-arrow');
+dropArrow.onclick=function(){
+    dropArrow.classList.toggle('d-a')
+    dropArrow.classList.toggle('drop-down-onclick');
+    noHover();
+}
+function noHover(){
+    const noHovers=document.getElementById('Sales-dropdown');
+    noHovers.classList.toggle('hover')
+    noHovers.classList.toggle('no-hover')
+}
