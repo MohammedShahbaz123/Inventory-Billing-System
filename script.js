edbef1be536cc619a4d3417ea1313bddfeea29f8
Mohammed Shahaz <134310109+MohammedShahbaz123@users.noreply.github.com>
2025-07-29 20:44:59 +0530

    Make Business Option Drop Down visible on click

--- script.js
+++ script.js
@@ -1,4 +1,5 @@
 const dropArrow=document.getElementById('drop-arrow');
+
 dropArrow.onclick=function(){
     dropArrow.classList.toggle('d-a')
     dropArrow.classList.toggle('drop-down-onclick');
@@ -8,4 +9,8 @@ function noHover(){
     const noHovers=document.getElementById('Sales-dropdown');
     noHovers.classList.toggle('hover')
     noHovers.classList.toggle('no-hover')
+}
+function businessDropDown(){
+    const switchBusinessOption=document.getElementById('switch-business-id')
+    switchBusinessOption.classList.toggle('business-active');
 }
